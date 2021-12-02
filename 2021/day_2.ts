7c60c12a6a230ffb06dfb4d1051b3e03e62230e0
Im-Beast <franik.mateusz@gmail.com>
2021-12-02 08:40:23 +0100

    day 2 completed

---
+++ 2021/day_2.ts
@@ -0,0 +1,63 @@
+import { input } from "./day_2_input.ts";
+
+function partOne(): number {
+  const position = {
+    horiz: 0,
+    depth: 0,
+  };
+
+  for (const move of input) {
+    const [command, am] = move.split(" ");
+    const amount = Number(am);
+
+    switch (command) {
+      case "forward":
+        position.horiz += amount;
+        break;
+      case "down":
+        position.depth += amount;
+        break;
+      case "up":
+        position.depth -= amount;
+        break;
+    }
+  }
+
+  return position.horiz * position.depth;
+}
+
+function partTwo(): number {
+  const position = {
+    horiz: 0,
+    depth: 0,
+    aim: 0,
+  };
+
+  for (const move of input) {
+    const [command, am] = move.split(" ");
+    const amount = Number(am);
+
+    switch (command) {
+      case "forward":
+        position.horiz += amount;
+        position.depth += position.aim * amount;
+        break;
+      case "down":
+        position.aim += amount;
+
+        break;
+      case "up":
+        position.aim -= amount;
+        break;
+    }
+  }
+
+  return position.horiz * position.depth;
+}
+
+console.log(
+  `
+Part one: ${partOne()}
+Part two: ${partTwo()}
+`,
+);
