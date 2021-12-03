aded2e5437459c3d17426b1e0f35defb045084f6
Im-Beast <franik.mateusz@gmail.com>
2021-12-03 12:15:15 +0100

    day 3 passed

---
+++ 2021/day_3.ts
@@ -0,0 +1,61 @@
+import { input } from "./day_3_input.ts";
+
+function partOne(): number {
+  const ones: number[] = [];
+  const zeros: number[] = [];
+
+  for (const bits of input) {
+    for (const [i, bit] of bits.split("").entries()) {
+      ones[i] ||= 0;
+      zeros[i] ||= 0;
+
+      if (bit === "1") {
+        ++ones[i];
+      } else {
+        ++zeros[i];
+      }
+    }
+  }
+
+  let gammaRate = "";
+  let epsilonRate = "";
+  for (const num in ones) {
+    gammaRate += ones[num] > zeros[num] ? "1" : "0";
+    epsilonRate += ones[num] > zeros[num] ? "0" : "1";
+  }
+
+  return parseInt(gammaRate, 2) * parseInt(epsilonRate, 2);
+}
+
+function partTwo(): number {
+  const getData = (reversed: boolean): number => {
+    let array = [...input];
+
+    for (let i = 0; array.length > 1; ++i) {
+      const amounts = { "0": 0, "1": 0 };
+
+      for (const bits of array) {
+        ++amounts[bits[i] as "0" | "1"];
+      }
+
+      const common = amounts["1"] >= amounts["0"] ? "1" : "0";
+      const filter = (value: string) =>
+        reversed ? value[i] !== common : value[i] === common;
+      if (array.some(filter)) {
+        array = array.filter(filter);
+      }
+    }
+
+    return parseInt(array[0], 2);
+  };
+
+  const oxygenGeneratorRating = getData(false);
+  const co2ScrubberRating = getData(true);
+
+  return oxygenGeneratorRating * co2ScrubberRating;
+}
+
+console.log(`
+Part one: ${partOne()}
+Part two: ${partTwo()}
+`);
